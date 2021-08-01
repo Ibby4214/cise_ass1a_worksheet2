8afb20d99c805fa6981dea849a19cd114b5e3d0e
ibby4214 <you@example.com>
2021-08-01 17:38:38 +1200

    first commmit

---
+++ backend/dummydata/articles.js
@@ -0,0 +1,35 @@
+const articles = [ 
+
+    {
+        _id: "1",
+        title: "An experimental evaluation of test driven development vs test-last development with industry professional", 
+        author: "Munir,H., K,Petersen, K., Moavved, M.",
+        source: "Ease",
+        pubyear: "2014", 
+        doi: "https://doi.org/10.1145/2601248.2601267",
+        
+    }, 
+
+    {
+        _id: "2",
+        title: "Realising quality improvement through test driven development: results and experiences of four industrial teams",
+        author: "Nagappan, N., Maximillien, E.M., Bhat, T.,William, L.",
+        source: "Empirical software engineering, 13(3), 289-302",
+        pubyear: "2008",
+        doi: "https://doi.org/10.1007/s10664-008-9062-z",
+        
+    }, 
+
+    {
+        _id: "3",
+        title: "Does Test-Driven Really improve software design quality",
+        author: "Janzen, D.S",
+        source: "Software, IEEE, 25(2) 77-84",
+        pubyear: "2008" ,
+        doi: "",
+
+    }, 
+
+];
+
+module.exports = articles; 
